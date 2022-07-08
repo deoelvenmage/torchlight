3b30a3e4515b4167edc7666febd97dc921ebd76f
deoelvenmage <deoelvenmage@gmail.com>
2022-07-08 14:50:10 -0600

    Made a basic darkness effect

--- torchlight.py
+++ torchlight.py
@@ -1,14 +1,14 @@
-# Important consts: Square size and maze size. Screen size = maze size * square size.
-# Alternatively, only grid-square-size, and currentMapSize is a member/global variable that gets updated with every level, getting bigger
-
 # Imports
+import math
 import arcade
 import random
+
+from attr import has
 
 # Constants
 SPRITE_SIZE = 16
 MAZE_SIZE = 20
-SCALING = 2.0 # I predict unpredictable behavior if this does nt even divide both sprite and maze size
+SCALING = 2.0 # I predict unpredictable behavior if this does not evenly divide both sprite and maze size
 SCALING_INT = int(SCALING)
 SCREEN_WIDTH = SPRITE_SIZE * MAZE_SIZE * SCALING_INT
 SCREEN_HEIGHT = SPRITE_SIZE * MAZE_SIZE * SCALING_INT
@@ -28,9 +28,11 @@ class Torchlight(arcade.Window):
         super().__init__(width, height, title)
 
         # Set up the empty sprite lists
-        self.wall_list = arcade.SpriteList()
-        # self.fog_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        self.fog_list = arcade.SpriteList()
         self.all_sprites = arcade.SpriteList()
+
+        self.physics_engine = None
         self.paused = False
     
     def setup(self): # TODO big setup in here; call generateMaze with MazeSize. Or just start it right now. Nah, make generator.
@@ -42,14 +44,14 @@ class Torchlight(arcade.Window):
         arcade.set_background_color((219, 168, 140))
 
         # Set up the player
-        self.player = arcade.Sprite("./images/skull_v2_1.png", SCALING)
+        self.player = arcade.Sprite("./images/skull_v2_1.png", SCALING) #TODO Set hitbox here to be 16x16; will center the sprite?
         self.player.top = self.height
         self.player.left = 0
         self.all_sprites.append(self.player)
 
         self.end_tile = arcade.Sprite("./images/end_tile.png", SCALING)
         self.end_tile.left = 0
-        self.end_tile.top = 0
+        self.end_tile.bottom = 0
         self.all_sprites.append(self.end_tile)        
 
         # Set up the maze
@@ -61,7 +63,6 @@ class Torchlight(arcade.Window):
             similar with Current and End.           
             """
 
-            cell_size = SPRITE_SIZE #pixels; todo remove if not needed
             ms = MAZE_SIZE # rows and columns
             visited_cells = []
             walls = []
@@ -82,7 +83,7 @@ class Torchlight(arcade.Window):
                             y = row * SPRITE_SIZE * SCALING_INT
 
                             sprite.left = x
-                            sprite.top = y
+                            sprite.bottom = y #was top; does this fix the upper funny? Yes
                             self.wall_list.append(sprite)
                             self.all_sprites.append(sprite)
 
@@ -107,12 +108,12 @@ class Torchlight(arcade.Window):
 
             #StartingPoint
 
-            scr = random.randint(1, ms)
-            scc = random.randint(1, ms)
+            scr = random.randint(0, ms-1)
+            scc = random.randint(0, ms-1)
 
             # Change player position
             self.player.left = scc * SPRITE_SIZE * SCALING_INT
-            self.player.top = scr * SPRITE_SIZE * SCALING_INT
+            self.player.bottom = scr * SPRITE_SIZE * SCALING_INT #changed like wall
 
             ccr, ccc = scr, scc
 
@@ -121,7 +122,7 @@ class Torchlight(arcade.Window):
             while not finished:
                 visitable_neighbours = check_neighbours(ccr, ccc)
                 if len(visitable_neighbours) != 0:
-                    d = random.randint(1, len(visitable_neighbours))-1
+                    d = random.randint(0, len(visitable_neighbours)-1)
                     ncr, ncc = visitable_neighbours[d]
                     map[ncr][ncc] = 'Open'
                     visited_cells.append([ncr, ncc])
@@ -134,20 +135,43 @@ class Torchlight(arcade.Window):
                         finished = True
 
             create()
-            end = random.randint(1, len(revisited_cells))-1
-            ecr = revisited_cells[end][0]
-            ecc = revisited_cells[end][1]
+            end = random.randint(0, len(revisited_cells)) 
+            ecr = revisited_cells[end][0] 
+            ecc = revisited_cells[end][1] 
+
+            """Important note regarding end and ecr stuff:"""
+            # index out of range error when it was at 1 and outside, empty range on inside
+            # index out of range here; it's because revisitedcells it's *empty*. no[0][0]. Re-check numbergen
+            # also, changing the thing to no longer use revisitedcells will fix it; the new-style end gen.
+            # so that'll be fine; don't need revisitedcells once I change that gen technique. corridor_end_cells; don't need to track direction, I think.
+            # But yes, this error will be fixed by changing the gen technique for be more fully my own.
 
             # change end tile position
             self.end_tile.left = ecc * SPRITE_SIZE * SCALING_INT
-            self.end_tile.top = ecr * SPRITE_SIZE * SCALING_INT
+            self.end_tile.bottom = ecr * SPRITE_SIZE * SCALING_INT #changed like wall
         
         create_maze()
 
-        # self.physics_engine = arcade.PhysicsEngineSimple(self.player, self.wall_list)
-        # TODO set up PhysicEngine, which will just prevent the player from going through walls. May need to look at hitboxAlgorithm parameter
+        def create_fog():
+            for row in range(MAZE_SIZE):
+                    for col in range(MAZE_SIZE):
+                        sprite = arcade.Sprite("./images/fog_tile.png", SCALING)
+
+                        x = col * SPRITE_SIZE * SCALING_INT
+                        y = row * SPRITE_SIZE * SCALING_INT
+
+                        sprite.left = x
+                        sprite.bottom = y
+                        self.fog_list.append(sprite)
+                        self.all_sprites.append(sprite)
+        
+        create_fog()
+
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player, self.wall_list) 
+        #TODO This does some funny shunting now. Maybe consider fixing after I make the sprite hitbox right and change the step size from half.
     
-    def on_key_press(self, symbol, modifiers): #Make this call update (turn_update?) with a parameter of 'command'; we'll use this over on_update
+    #Make this call update (turn_update?) with a parameter of 'command'; we'll use this over on_update
+    def on_key_press(self, symbol, modifiers): 
         """Handle user keyboard input
         Q: Quit the game
         P: Pause/Unpause the game
@@ -161,44 +185,18 @@ class Torchlight(arcade.Window):
         if symbol == arcade.key.Q:
             # Quit immediately
             arcade.close_window()
-
         if symbol == arcade.key.P:
             self.paused = not self.paused
 
-        if symbol == arcade.key.I or symbol == arcade.key.UP:
-            self.player.change_y = 5
-
-        if symbol == arcade.key.K or symbol == arcade.key.DOWN:
-            self.player.change_y = -5
-
-        if symbol == arcade.key.J or symbol == arcade.key.LEFT:
-            self.player.change_x = -5
-
-        if symbol == arcade.key.L or symbol == arcade.key.RIGHT:
-            self.player.change_x = 5
-
-    def on_key_release(self, symbol: int, modifiers: int): #This is removable cause we dodn't do key release
-        """Undo movement vectors when movement keys are released
-
-        Arguments:
-            symbol {int} -- Which key was pressed
-            modifiers {int} -- Which modifiers were pressed
-        """
-        if (
-            symbol == arcade.key.I
-            or symbol == arcade.key.K
-            or symbol == arcade.key.UP
-            or symbol == arcade.key.DOWN
-        ):
-            self.player.change_y = 0
-
-        if (
-            symbol == arcade.key.J
-            or symbol == arcade.key.L
-            or symbol == arcade.key.LEFT
-            or symbol == arcade.key.RIGHT
-        ):
-            self.player.change_x = 0
+        # top and right chosen because positive in those directions is positive x and y in arcade's positioning
+        if symbol == arcade.key.W or symbol == arcade.key.UP:
+            self.player.top += SPRITE_SIZE * SCALING_INT // 2 # Will always come out because sprite size will always be a 2-power
+        if symbol == arcade.key.S or symbol == arcade.key.DOWN:
+            self.player.top -= SPRITE_SIZE * SCALING_INT // 2 # However, this is just a temorary fix for the shunting problem cause by sprite being top left
+        if symbol == arcade.key.A or symbol == arcade.key.LEFT:
+            self.player.right -= SPRITE_SIZE * SCALING_INT // 2 # Hitbox is smaller than 16px; try centering it first.
+        if symbol == arcade.key.D or symbol == arcade.key.RIGHT:
+            self.player.right += SPRITE_SIZE * SCALING_INT // 2
 
     def on_update(self, delta_time: float):
         """Update the positions and statuses of all game objects
@@ -217,7 +215,8 @@ class Torchlight(arcade.Window):
             arcade.close_window() 
     
         # Update everything
-        self.all_sprites.update()
+        self.physics_engine.update() # Easiest thing of my life what the heck
+        # self.all_sprites.update() not needed since we're using a physics engine now
     
         # Keep the player on screen
         if self.player.top > self.height:
@@ -228,12 +227,18 @@ class Torchlight(arcade.Window):
             self.player.bottom = 0
         if self.player.left < 0:
             self.player.left = 0
+        
+        for square in self.fog_list:
+            if ((abs(square.center_x - self.player.center_x) + abs(square.center_y - self.player.center_y)) <= 3 * SPRITE_SIZE * SCALING) and (arcade.has_line_of_sight(self.player.position, square.position, self.wall_list)):
+                square.alpha = 0
+            else:
+                square.alpha = 255
     
     def on_draw(self):
         """Draw all game objects
         """
         arcade.start_render()
-        self.all_sprites.draw()
+        self.all_sprites.draw() #Maybe here put the check if LoS && difference between sprite center and player center is <= SpriteSize*Scalaing*3 (3 for 3 squares)
             
 def main():
     """ Main function """
